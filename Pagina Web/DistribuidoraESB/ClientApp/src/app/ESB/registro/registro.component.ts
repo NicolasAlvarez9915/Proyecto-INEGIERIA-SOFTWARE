6897a0c5c6ddd1b0c62af1ab8db60df70ea63523
jeinerrobles <nicoalvarez9915@gmail.com>
2020-11-05 09:12:54 -0500

    Registra Cliente

--- Pagina Web/DistribuidoraESB/ClientApp/src/app/ESB/registro/registro.component.ts
+++ Pagina Web/DistribuidoraESB/ClientApp/src/app/ESB/registro/registro.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UseExistingWebDriver } from 'protractor/built/driverProviders';
+import { AlertModalComponent } from 'src/app/@base/alert-modal/alert-modal.component';
 import { ClienteService } from 'src/app/services/cliente.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { Cliente } from '../Models/cliente';
@@ -76,6 +77,64 @@ export class RegistroComponent implements OnInit {
   }
 
   Registrar(){
-    this.router.navigate(['/Perfil']);
+
+    this.cliente = this.formularioRegistro.value;
+    this.usuario.contraseña = this.formularioRegistro.value.contrasenaConfirmar;
+    this.usuario.correo = this.formularioRegistro.value.correo;
+    this.usuario.idPersona = this.cliente.identificacion;
+    this.usuario.rol = 'Cliente';
+    if(this.formularioRegistro.value.contrasenaconfirmar != this.formularioRegistro.value.contrasena )
+    {
+      const messageBox = this.modalService.open(AlertModalComponent)
+      messageBox.componentInstance.title = "ALERTA";
+      messageBox.componentInstance.message = "Las contraseñas no coninciden";
+
+    }else
+    {
+      this.clienteService.buscar(this.cliente.identificacion).subscribe(
+        r =>
+        {
+          if(r!=null)
+          {
+            const messageBox = this.modalService.open(AlertModalComponent)
+            messageBox.componentInstance.title = "ALERTA";
+            messageBox.componentInstance.message = "Ya existe un cliente registrado con esta identificacion";
+          }else
+          {
+            this.usuarioService.validarSession(this.usuario.correo).subscribe(r=>
+              {
+                if(r != null )
+                {
+                  const messageBox = this.modalService.open(AlertModalComponent)
+            messageBox.componentInstance.title = "ALERTA";
+            messageBox.componentInstance.message = "Ya existe un cliente registrado con este correo";
+                }else{
+                  this.clienteService.post(this.cliente).subscribe
+            (
+              r =>
+              {
+                
+                this.usuarioService.post(this.usuario).subscribe(
+                  r=>{
+                    if(r!=null)
+                    {
+                      const messageBox = this.modalService.open(AlertModalComponent)
+                      messageBox.componentInstance.title = "BIEN HECHO.";
+                      messageBox.componentInstance.message = "Cliente registrado. Cuenta de cliente creada.";
+                      this.usuarioService.GuardarUsuarioSesion(this.usuario);
+                      this.router.navigate(['/Perfil']);
+                    }
+                  }
+                )
+              }
+            );
+                }
+              })
+  
+          }
+        }
+      );
+    }
+    
   }
 }
